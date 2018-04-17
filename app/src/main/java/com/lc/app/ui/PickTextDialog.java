bb4aa4f3fa37e32c4907940084bcd3222a8d45db
addskya@163.com <addskya@163.com>
2018-04-17 21:43:05 +0800

    bug

---
+++ app/src/main/java/com/lc/app/ui/PickTextDialog.java
@@ -0,0 +1,25 @@
+package com.lc.app.ui;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.lc.app.BaseDialog;
+
+/**
+ * Created by Orange on 18-4-17.
+ * Email:addskya@163.com
+ */
+public class PickTextDialog extends BaseDialog {
+
+
+    public PickTextDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    @Override
+    protected View onCreateView(LayoutInflater inflater) {
+        return null;
+    }
+}
